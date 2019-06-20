eab8e6259a5bcf7137a1ea38e8465b8e7d91d6c0
Mario <marioquiro17@gmail.com>
2019-06-20 18:55:58 -0300

    include files .h

--- main/main.ino
+++ main/main.ino
@@ -1,74 +1,11 @@
-  //variable para detectar el modo del reloj 
-  //True en modo de enrolamiento
-  //False en modo de registro de fichadas
-
-
-
-//variables para el servidor web
-#include <SPI.h>
-#include <Ethernet.h>
 #include "fingerprint/FingerPrint.h"
 
-boolean  is_enrrol = false;
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network:
-byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
-};
-IPAddress ip(192, 168, 1, 177);
 
-// Initialize the Ethernet server library
-// with the IP address and port you want to use
-// (port 80 is default for HTTP):
-EthernetServer server(80);
-
-FingerPrint finger();
-
-void setup() {
-
-  // Open serial communications and wait for port to open:
-  Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
-  Serial.println("Ethernet WebServer Example");
-
-  // start the Ethernet connection and the server:
-  Ethernet.begin(mac, ip);
-
-  // Check for Ethernet hardware present
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
-    Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
-    while (true) {
-      delay(1); // do nothing, no point running without Ethernet hardware
-    }
-  }
-  if (Ethernet.linkStatus() == LinkOFF) {
-    Serial.println("Ethernet cable is not connected.");
-  }
-
-  // start the server
-  server.begin();
-  Serial.print("server is at ");
-  Serial.println(Ethernet.localIP());
-
-  
+void setup()  
+{
 
 }
-
-void loop() {
-    if (is_enrol){
-    finger.enrrol();
-    is_enrrol= false;
-  }else{
-    huella.readfingr();
-    tarjeta.readcard();
-    
-  }
-
-  if(){
-    
-  }
-  
-
+void loop()                     // run over and over again
+{
+  enrrol(4);
 }
